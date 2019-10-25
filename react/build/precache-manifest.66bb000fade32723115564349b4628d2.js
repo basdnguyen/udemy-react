a0a6d71c69e13a4a99f2d3eb1b71861e1fe2d879
Bang Nguyen <bang.nguyen@go1.com>
2019-10-25 20:07:56 +0700

    split projects

---
+++ react/build/precache-manifest.66bb000fade32723115564349b4628d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38c9c0d7fc2ea35002e285373bc7125",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9e8a9f183262443dbb7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5a184f086ba5e6f7e28a",
+    "url": "/static/js/2.bb305f48.chunk.js"
+  },
+  {
+    "revision": "c9e8a9f183262443dbb7",
+    "url": "/static/js/main.970b3471.chunk.js"
+  },
+  {
+    "revision": "152d784f588fae3c0634",
+    "url": "/static/js/runtime~main.fbde1adf.js"
+  }
+]);
